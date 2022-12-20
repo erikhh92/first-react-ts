da51fda4beb65b172df29fdfdba54a9629025e0f
Eric Molina <emolina@2ionline.com>
2022-12-20 11:53:31 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import List from "./components/List";
+import { Sub } from "./types";
+
+interface AppState {
+  subs: Array<Sub>,
+  newSubsNumber: number
+}
 
 function App() {
+  const [subs, setSubs] = useState<AppState["subs"]>([]);
+  const [newSubsNumber, setNewSubsNumber] = useState<AppState["newSubsNumber"]>(0);
+  const divRef = useRef<HTMLDivElement>(null)
+  
+  useEffect(() => {
+    
+  }, []);
+
+  const handleNewSub = (newSub: Sub): void => {
+    setSubs(subs => [...subs, newSub])
+    setNewSubsNumber(n => n + 1)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={divRef}>
+      <h1>Midu subs</h1>
+      <List subs={subs} />
+      New subs: {newSubsNumber}
+      <Form onNewSub={handleNewSub}/>
     </div>
   );
 }
